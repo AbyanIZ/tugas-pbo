3a4b12fc719719e1d127003ff8b73e17e89f88a4
AbyanIZ <yann1232008@gmail.com>
2024-12-04 14:51:10 +0700

    food

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,15 +13,12 @@ class MyApp extends StatelessWidget {
       title: 'Food App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Homepage(),
-      routes: {
-        '/CartPage': (context) => CartPage(),
-      },
     );
   }
 }
 
 class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+  const Homepage({Key? key}) : super(key: key);
 
   @override
   _HomepageState createState() => _HomepageState();
@@ -30,14 +28,16 @@ class _HomepageState extends State<Homepage> {
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
-    if (index == 1) {
-      Navigator.pushNamed(context, '/CartPage');
-    } else {
-      setState(() {
-        _selectedIndex = index;
-      });
-    }
+    setState(() {
+      _selectedIndex = index;
+    });
   }
+
+  final List<Widget> _pages = [
+    HomeScreen(),
+    CartPage(),
+    OrderPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +46,7 @@ class _HomepageState extends State<Homepage> {
         backgroundColor: Colors.white,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.black),
+          icon: Icon(Icons.menu, color: const Color.fromARGB(255, 255, 255, 255)),
           onPressed: () {},
         ),
         actions: [
@@ -56,7 +56,7 @@ class _HomepageState extends State<Homepage> {
           ),
         ],
       ),
-      body: HomeScreen(),
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
@@ -108,7 +108,7 @@ class _HomeScreenState extends State<HomeScreen> {
             'All Food',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 20),
           Expanded(
             child: GridView.builder(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -117,7 +117,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 crossAxisSpacing: 10,
                 mainAxisSpacing: 10,
               ),
-              itemCount: 4,
+              itemCount: 5,
               itemBuilder: (context, index) {
                 return FoodCard();
               },
@@ -142,7 +142,7 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: EdgeInsets.all(10),
             decoration: BoxDecoration(
               color: isSelected ? Colors.blue : Colors.white,
-              borderRadius: BorderRadius.circular(15),
+              borderRadius: BorderRadius.circular(10),
               boxShadow: [
                 if (isSelected)
                   BoxShadow(
@@ -153,7 +153,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
               ],
             ),
+            child: ClipRRect(
             child: Image.asset(imagePath, width: 50, height: 50),
+            borderRadius: BorderRadius.circular(20),
+            )
           ),
           SizedBox(height: 5),
           Text(
@@ -168,6 +171,7 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
 class FoodCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -177,15 +181,12 @@ class FoodCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start, // Aligns content to the left
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-            child: Image.asset(
-              'images/burger.jpg',
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: 180,
+          Expanded(
+            child: ClipRRect(
+              child: Image.asset('images/burger.jpg', fit: BoxFit.cover),
+              borderRadius: BorderRadius.circular(40),
             ),
           ),
           Padding(
@@ -195,13 +196,11 @@ class FoodCard extends StatelessWidget {
               children: [
                 Text(
                   'Burger King Medium',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal), // Adjusted font weight
-                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
                 Text(
                   'Rp. 50.000,00',
                   style: TextStyle(fontSize: 14, color: Colors.grey),
-                  textAlign: TextAlign.left,
                 ),
               ],
             ),
@@ -224,10 +223,399 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cart"),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          "Cart",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: Center(
-        child: Text("Cart Page"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  CartItem(
+                    imagePath: 'images/burger.jpg',
+                    name: 'Burger King Medium',
+                    price: 50,
+                    quantity: 1,
+                  ),
+                  CartItem(
+                    imagePath: 'images/teh.jpg',
+                    name: 'Teh Botol',
+                    price: 40,
+                    quantity: 2,
+                  ),
+                  CartItem(
+                    imagePath: 'images/burger.jpg',
+                    name: 'Burger King Small',
+                    price: 35,
+                    quantity: 1,
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Column(
+                children: [
+                  SummaryRow(label: 'PPN 11%', value: 10),
+                  SummaryRow(label: 'Total Belanja', value: 94),
+                  Divider(),
+                  SummaryRow(
+                    label: 'Total Pembayaran',
+                    value: 104,
+                    isBold: true,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 16),
+              ),
+              child: Center(
+                child: Text(
+                  'Checkout',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//CartItem
+
+class CartItem extends StatelessWidget {
+  final String imagePath;
+  final String name;
+  final int price;
+  final int quantity;
+
+  CartItem({
+    required this.imagePath,
+    required this.name,
+    required this.price,
+    required this.quantity,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              imagePath,
+              width: 80,
+              height: 80,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  'Rp. ${price.toString()}.000,00',
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.remove_circle_outline, color: Colors.grey),
+              ),
+              Text('$quantity'),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add_circle_outline, color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SummaryRow extends StatelessWidget {
+  final String label;
+  final int value;
+  final bool isBold;
+
+  SummaryRow({
+    required this.label,
+    required this.value,
+    this.isBold = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontSize: 16, fontWeight: isBold ? FontWeight.bold : FontWeight.normal),
+          ),
+          Text(
+            'Rp. $value.000,00',
+            style: TextStyle(fontSize: 16, fontWeight: isBold ? FontWeight.bold : FontWeight.normal),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          "Orders",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 16),
+              ),
+              icon: Icon(Icons.add),
+              label: Text("Add Data"),
+            ),
+            SizedBox(height: 20),
+            Container(
+              color: Colors.grey[200],
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(flex: 2, child: Text("Foto", style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(flex: 4, child: Text("Nama Produk", style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(flex: 3, child: Text("Harga", style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(flex: 1, child: Text("Aksi", style: TextStyle(fontWeight: FontWeight.bold))),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  OrderItem(
+                    imagePath: 'images/burger.jpg',
+                    name: 'Burger King Medium',
+                    price: 50,
+                  ),
+                  OrderItem(
+                    imagePath: 'images/teh.jpg',
+                    name: 'Teh Botol',
+                    price: 4,
+                  ),
+                  OrderItem(
+                    imagePath: 'images/burger.jpg',
+                    name: 'Burger King Small',
+                    price: 35,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// PesanPage
+
+class PesanPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          "Orders",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person, color: Colors.black),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () {
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 16),
+              ),
+              icon: Icon(Icons.add),
+              label: Text("Add Data"),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                children: [
+                  Expanded(flex: 2, child: Text("Foto", style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(flex: 4, child: Text("Nama Produk", style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(flex: 3, child: Text("Harga", style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(flex: 1, child: Text("Aksi", style: TextStyle(fontWeight: FontWeight.bold))),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: ListView(
+                children: [
+                  OrderItem(
+                    imagePath: 'images/burger.jpg',
+                    name: 'Burger King Medium',
+                    price: 50,
+                  ),
+                  OrderItem(
+                    imagePath: 'images/teh.jpg',
+                    name: 'Teh Botol',
+                    price: 4,
+                  ),
+                  OrderItem(
+                    imagePath: 'images/burger.jpg',
+                    name: 'Burger King Small',
+                    price: 35,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OrderItem extends StatelessWidget {
+  final String imagePath;
+  final String name;
+  final int price;
+
+  OrderItem({
+    required this.imagePath,
+    required this.name,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(
+                imagePath,
+                width: 60,
+                height: 60,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(
+                name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Text(
+              'Rp. ${price.toString()}.000,00',
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+          ),
+
+          Expanded(
+            flex: 1,
+            child: IconButton(
+              onPressed: () {
+              },
+              icon: Icon(Icons.delete, color: Colors.red),
+              iconSize: 24,
+            ),
+          ),
+        ],
       ),
     );
   }
